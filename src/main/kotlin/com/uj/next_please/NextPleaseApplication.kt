bd3c420a340f40c74b2e33857cf8b0c00fe3d8c3
mkosciolek <michal.kosc20@gmail.com>
2026-03-22 13:11:39 +0100

    setup project

---
+++ src/main/kotlin/com/uj/next_please/NextPleaseApplication.kt
@@ -0,0 +1,11 @@
+package com.uj.next_please
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextPleaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<NextPleaseApplication>(*args)
+}
